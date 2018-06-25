109a1a051c2f00c14e43a4706a8e0809db4bf7b9
kevin schuchard <schuchard.kevin@gmail.com>
2018-06-25 15:17:08 -0600

    add online$ status add fallback rocket image

--- src/app/core/services/app-status.service.ts
+++ src/app/core/services/app-status.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { CoreModule } from '../core.module';
 import { SwUpdate } from '@angular/service-worker';
 import { UpdateAvailableEvent, UpdateActivatedEvent } from '@angular/service-worker/src/low_level';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, merge, fromEvent, of } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 // import { interval } from 'rxjs';
 
 @Injectable({
@@ -12,7 +13,7 @@ export class AppStatusService {
   swUpdates = new BehaviorSubject('');
 
   constructor(private updates: SwUpdate) {
-    // programmatically check for updates
+    // programmatically check for updates every 5 seconds
     // interval(5000).subscribe(() => this.updates.checkForUpdate());
 
     this.updates.available.subscribe((event: UpdateAvailableEvent) => {
@@ -36,5 +37,13 @@ export class AppStatusService {
 
   get updateAvailable() {
     return this.swUpdates.asObservable();
+  }
+
+  get online() {
+    return merge(
+      of(navigator.onLine),
+      fromEvent(window, 'online').pipe(mapTo(true)),
+      fromEvent(window, 'offline').pipe(mapTo(false))
+    );
   }
 }
--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { AppStatusService } from '../core/services/app-status.service';
 
 @Component({
   templateUrl: './launch.component.html',
@@ -10,10 +11,13 @@ import { takeUntil } from 'rxjs/operators';
 export class LaunchComponent implements OnInit, OnDestroy {
   allLaunches: Observable<any>;
   destroy$ = new Subject();
+  online$ = true;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private appStatus: AppStatusService) {}
 
   ngOnInit() {
+    this.appStatus.online.pipe(takeUntil(this.destroy$)).subscribe((res) => (this.online$ = res));
+
     this.http
       .get<any>(`https://api.spacexdata.com/v2/launches`)
       .pipe(takeUntil(this.destroy$))
